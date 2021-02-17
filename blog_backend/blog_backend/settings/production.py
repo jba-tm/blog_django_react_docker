c8efaa2be512e0163c518b5d4b16ccf4752a5025
jba-tm <jumageldiyewb@list.ru>
2021-02-17 12:49:59 +0500

    Completed

--- blog_backend/blog_backend/settings/production.py
+++ blog_backend/blog_backend/settings/production.py
@@ -5,20 +5,11 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ORIGIN_WHITELIST").split(" ")
 
 try:
     from .local import *
---
+++ blog_frontend/Dockerfile
@@ -0,0 +1,6 @@
+FROM node:14.4.0-alpine3.10
+USER root
+WORKDIR /frontend
+COPY . /frontend
+RUN yarn
+RUN yarn build
